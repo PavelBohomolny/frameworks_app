cb037b33e153e8352311e7604f7d36a3fb34c392
NexusUA <pbogomolny93@gmal.com>
2022-10-26 13:57:13 +0200

    Initial Commit

---
+++ Apple Frameworks/Apple_FrameworksApp.swift
@@ -0,0 +1,17 @@
+//
+//  Apple_FrameworksApp.swift
+//  Apple Frameworks
+//
+//  Created by Pavel Bohomolnyi on 26/10/2022.
+//
+
+import SwiftUI
+
+@main
+struct Apple_FrameworksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
